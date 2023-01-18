db8ba33763a5169413c0ff7bd321e907d55a5ad0
Andrew Malcolm <andrewmalcolm.nz@gmail.com>
2023-01-18 15:23:56 +1300

    added client

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios").default
 const { randomString, timeout } = require("./utils")
+const url = require("node:url")
 
 const config = {
 	port: 9000,
@@ -26,6 +27,54 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+
+app.get("/authorize", (req, res) => {
+	state = randomString()
+
+	const redirectUrl = url.parse(config.authorizationEndpoint)
+	redirectUrl.query = {
+		response_type: "code",
+		client_id: config.clientId,
+		redirect_uri: config.redirectUri,
+		scope: "permission:name permission:date_of_birth",
+		state
+	}
+
+	res.redirect(url.format(redirectUrl))
+})
+
+app.get("/callback", (req, res) => {
+
+	if (state !== req.query.state) {
+		res.status(403).send("Error: state mismatch")
+		return
+	}
+
+	axios({
+		method: "POST",
+		url: config.tokenEndpoint,
+		auth: {
+			username: config.clientId,
+			password: config.clientSecret
+		},
+		data: {
+			code: req.query.code
+		}
+	}).then(response => {
+		return axios({
+			method: "GET",
+			url: config.userInfoEndpoint,
+			headers: {
+				authorization: "bearer " + response.data.access_token
+			}
+		})
+	}).then(response => {
+		res.render("welcome", { user: response.data })
+	}).catch(err => {
+		console.error(err)
+		res.status(500).send("Error: something went wrong")
+	})
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
